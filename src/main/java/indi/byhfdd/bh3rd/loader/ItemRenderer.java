ba1b3624591afcf0b3da7030934743f235a446dd
byhfdd <s922277170@outlook.com>
2025-08-06 15:51:22 +0800

    hf

--- src/main/java/indi/byhfdd/bh3rd/loader/ItemRenderer.java
+++ src/main/java/indi/byhfdd/bh3rd/loader/ItemRenderer.java
@@ -17,61 +17,56 @@ public class ItemRenderer  implements IItemRenderer {
         this.name = name;
     }
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
-        return true;
+        return type == ItemRenderType.EQUIPPED || type == ItemRenderType.EQUIPPED_FIRST_PERSON;
     }
 
     public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
-        return type == ItemRenderType.ENTITY && (helper == ItemRendererHelper.ENTITY_BOBBING || helper == ItemRendererHelper.ENTITY_ROTATION);
+        return false;
     }
 
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
-        if (this.modelobj == null) {
-            this.modelobj = (WavefrontObject) AdvancedModelLoader.loadModel(new ResourceLocation("bh3rd:model/"+ this.name +".obj"));
-            this.tex = new ResourceLocation("bh3rd:textures/weapon/"+ this.name +".png");
-        } else {
-            GL11.glDisable(2896);
-            GL11.glDisable(2884);
+        // 只在手持时渲染自定义模型
+        if (type == ItemRenderType.EQUIPPED || type == ItemRenderType.EQUIPPED_FIRST_PERSON) {
+
+            if (this.modelobj == null) {
+                try {
+                    this.modelobj = (WavefrontObject) AdvancedModelLoader.loadModel(
+                        new ResourceLocation("bh3rd:model/" + this.name + ".obj")
+                    );
+                    this.tex = new ResourceLocation("bh3rd:textures/weapon/" + this.name + ".png");
+                } catch (Exception e) {
+                    System.err.println("Failed to load model or texture for: " + this.name);
+                    e.printStackTrace();
+                    return; // 加载失败则不渲染
+                }
+            }
+
+            GL11.glDisable(2896); // 禁用光照
+            GL11.glDisable(2884); // 禁用背面剔除
+
             float size = 2.0F;
+
+            GL11.glPushMatrix();
+
             if (type == ItemRenderType.EQUIPPED) {
-                GL11.glPushMatrix();
                 GL11.glTranslatef(0.83F, 0.15F, -0.05F);
                 GL11.glScalef(size, size, size);
                 GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
                 GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                this.mc.renderEngine.bindTexture(this.tex);
-                this.modelobj.renderAll();
-                GL11.glPopMatrix();
-            } else if (type == ItemRenderType.INVENTORY) {
-                GL11.glPushMatrix();
-                GL11.glTranslatef(12.0F, 4.0F, -0.0F);
-                GL11.glScalef(size * 9.0F, size * 9.0F, size * 9.0F);
-                GL11.glRotatef(40.0F, 0.0F, 0.0F, 1.0F);
-                GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                this.mc.renderEngine.bindTexture(this.tex);
-                this.modelobj.renderAll();
-                GL11.glPopMatrix();
             } else if (type == ItemRenderType.EQUIPPED_FIRST_PERSON) {
-                GL11.glPushMatrix();
                 GL11.glTranslatef(0.9F, 0.2F, -0.0F);
                 GL11.glScalef(size, size, size);
                 GL11.glRotatef(30.0F, 0.0F, 0.0F, 1.0F);
                 GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                this.mc.renderEngine.bindTexture(this.tex);
-                this.modelobj.renderAll();
-                GL11.glPopMatrix();
-            } else if (type == ItemRenderType.ENTITY) {
-                GL11.glPushMatrix();
-                GL11.glTranslatef(0.2F, -0.0F, 0.0F);
-                GL11.glScalef(size / 1.0F, size / 1.0F, size / 1.0F);
-                GL11.glRotatef(0.0F, 0.0F, 0.0F, 1.0F);
-                GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                this.mc.renderEngine.bindTexture(this.tex);
-                this.modelobj.renderAll();
-                GL11.glPopMatrix();
             }
 
-            GL11.glEnable(2896);
-            GL11.glEnable(2884);
+            this.mc.renderEngine.bindTexture(this.tex);
+            this.modelobj.renderAll();
+
+            GL11.glPopMatrix();
+
+            GL11.glEnable(2896); // 启用光照
+            GL11.glEnable(2884); // 启用背面剔除
         }
 
     }
