c5af6dd93e3cb948862f2fe118d33640427f3930
byhfdd <s922277170@outlook.com>
2025-08-11 16:04:51 +0800

    hf

--- src/main/java/indi/byhfdd/bh3rd/Main.java
+++ src/main/java/indi/byhfdd/bh3rd/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public static final String MODID = "bh3rd";
     public static final String NAME = "Honkai Impact 3";
-    public static final String VERSION = "2.3.4";
+    public static final String VERSION = "2.3.5 ";
     public static final String dependencies = "required-after:manametalmod;after:Muya";
     @SidedProxy(clientSide = "indi.byhfdd.bh3rd.ClientProxy", serverSide = "indi.byhfdd.bh3rd.CommonProxy")
     public static CommonProxy proxy;
